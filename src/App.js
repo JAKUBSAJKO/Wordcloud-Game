08bd0923403c1805d66e75ecd8de069296ac1c17
Jakub Sajko <jakubsajko.inf@gmail.com>
2022-10-09 18:53:43 +0200

    [TASK-001]: Create 3 pages and create routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import Game from 'pages/Game/Game';
+import Result from 'pages/Result/Result';
+import Start from 'pages/Start/Start';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
-  return <div className="App">Test</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Start />} />
+          <Route path="/game" element={<Game />} />
+          <Route path="/result" element={<Result />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
