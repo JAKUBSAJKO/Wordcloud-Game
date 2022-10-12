f934379519ec2977ce0b07bfe24cf8a0c52731fd
Jakub Sajko <jakubsajko.inf@gmail.com>
2022-10-12 17:24:36 +0200

    [TASK-005: Create feature to check which picked word is correct]

--- src/contexts/gameContext.js
+++ src/contexts/gameContext.js
@@ -2,14 +2,40 @@ import { createContext, useState } from 'react';
 
 const gameContext = {
   username: '',
+  finishBtn: false,
+  pickedWords: [],
+  correctAnswers: [],
+  wrongAnswers: [],
+  noSelectedCorrectAnswers: [],
 };
 
 export const GameContext = createContext(gameContext);
 
 export const GameContextProvider = ({ children }) => {
   const [username, setUsername] = useState('');
+  const [finishGameBtn, setFinishGameBtn] = useState(false);
+  const [pickedWords, setPickedWords] = useState([]);
+  const [correctAnswers, setCorrectAnswers] = useState([]);
+  const [wrongAnswers, setWrongAnswers] = useState([]);
+  const [noSelectedCorrectAnswers, setNoSelectedCorrectAnswers] = useState([]);
+
   return (
-    <GameContext.Provider value={{ username, setUsername }}>
+    <GameContext.Provider
+      value={{
+        username,
+        setUsername,
+        finishGameBtn,
+        setFinishGameBtn,
+        pickedWords,
+        setPickedWords,
+        correctAnswers,
+        setCorrectAnswers,
+        wrongAnswers,
+        setWrongAnswers,
+        noSelectedCorrectAnswers,
+        setNoSelectedCorrectAnswers,
+      }}
+    >
       {children}
     </GameContext.Provider>
   );
